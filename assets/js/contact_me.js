bdadcec0b1f8e163099b155bd66208479d1e2699
bansalpratik0 <44139896+bansalpratik0@users.noreply.github.com>
2019-07-11 20:29:48 -0400

    Update contact_me.js

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -1,14 +1,22 @@
+
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-          var name = document.getElementById("name").value;
+$("input,textarea").jqBootstrapValidation({
+preventSubmit: true,
+submitError: function($form, event, errors) {
+// additional error messages or events
+},
+submitSuccess: function($form, event) {
+event.preventDefault(); // prevent default submit behaviour
+// get values from FORM
+var form = false;
+var length = document.forms.length;
+for(var i = 0; i < length; i++) {
+if(form.id == "contactform") {
+form = document.forms[i];
+}
+}
+var name = document.getElementById("name").value;
 var email = document.getElementById("email").value;
 var website = document.getElementById("website").value;
 var message = document.getElementById("comments").value;
@@ -22,45 +30,45 @@ data: {
 "website": website,
 "message": message
 },
-cache: false, 
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+cache: false,
+success: function() {
+// Success message
+$('#success').html("<div class='alert alert-success'>");
+$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+.append("</button>");
+$('#success > .alert-success')
+.append("<strong>Your message has been sent. </strong>");
+$('#success > .alert-success')
+.append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+//clear all fields
+$('#contactForm').trigger("reset");
+},
+error: function() {
+// Fail message
+$('#success').html("<div class='alert alert-danger'>");
+$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+.append("</button>");
+$('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that my mail server is not responding. Please try again later!");
+$('#success > .alert-danger').append('</div>');
+//clear all fields
+$('#contactForm').trigger("reset");
+},
+})
+},
+filter: function() {
+return $(this).is(":visible");
+},
+});
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
+$("a[data-toggle=\"tab\"]").click(function(e) {
+e.preventDefault();
+$(this).tab("show");
+});
 });
 
 
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
-    $('#success').html('');
-});
+$('#success').html('');
+}); 
